4db8990709f06ebee5086272d5f6bf3b619b82b2
memilianunez <marianaemilia.ar@gmail.com>
2023-10-11 10:53:11 -0300

    Se agregaron las rutas en el archivo App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import Navbar from "./components/Navbar";
+import Checkout from "./components/checkout";
 
 function App() {
-<h1>hola mundo</h1>
+    return (
+        <div>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<ItemListContainer />} />
+                <Route path="/category/:categoryId" element={<ItemListContainer />} />
+                <Route path="/item/:id" element={<ItemDetailContainer />} />
+            </Routes>
+
+            <Checkout />
+        </div>
+    );
 }
 
-export default App
+export default App;
