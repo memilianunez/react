32bc640186ae13f8f8d00da07d5a00490a29af29
memilianunez <marianaemilia.ar@gmail.com>
2023-10-11 10:40:39 -0300

    Se agregó el archivo Checkout.jsx

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,73 @@
+import { useContext, useState } from "react";
+import { CartContext } from "../context/CartContext";
+import { serverTimestamp } from "firebase/firestore";
+import { getCartTotal, mapCartToOrderItems } from "../utils";
+import { createOrder } from "../services";
+
+const Checkout = () => {
+    const [orderId, setOrderId] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const { cart, clear } = useContext(CartContext);
+
+    const total = getCartTotal(cart);
+
+    const handleCheckout = () => {
+        const order = {
+            buyer: {
+                name: "Juan Pepito",
+                phone: "0123456789",
+                email: "juanpepito@gmail.com",
+            },
+            items: mapCartToOrderItems(cart),
+            total,
+            date: serverTimestamp(),
+        };
+
+        setIsLoading(true);
+        createOrder(order).then((docRef) => {
+            setOrderId(docRef.id);
+            setIsLoading(false);
+            clear();
+        });
+    };
+
+    return (
+        <div>
+            <h1>Checkout</h1>
+
+            <h2>Resumen de compra</h2>
+
+            {orderId && <p>El id de tu compra es: {orderId}</p>}
+
+            {!orderId && (
+                <>
+                    <div>
+                        <h4>Formulario de contacto</h4>
+                    </div>
+
+                    <div>
+                        <h4>Productos</h4>
+                        <ul>
+                            {cart.map((item) => (
+                                <li key={item.id}>
+                                    <p>{item.title}</p>
+                                    <p>Cantidad: {item.quantity}</p>
+                                    <p>Precio por unidad: ${item.price}</p>
+                                    <p>Subtotal: ${item.price * item.quantity}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <p>Total: {total}</p>
+
+                    <button onClick={handleCheckout}>Finalizar compra</button>
+
+                    {isLoading && <p>Procesando compra...</p>}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Checkout;
