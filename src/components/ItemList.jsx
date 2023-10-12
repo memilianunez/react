94320d9287666e5ef5efab4d25300ba9b6cd9106
memilianunez <marianaemilia.ar@gmail.com>
2023-10-12 10:30:10 -0300

    Se eliminaron anotaciones del archivo maain.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,35 +1,70 @@
+// import propTypes from "prop-types";
+// import { Link } from "react-router-dom";
+
+// const ItemList = ({ items, isLoading }) => {
+//     if (isLoading) {
+//         return <h2>Cargando...</h2>;
+//     }
+
+//     return (
+//         <div>
+//             <h1>ItemList</h1>
+
+//             <Link to="/checkout">Comprar</Link>
+
+//             <ul>
+//                 {items.map((item) => (
+//                     <li key={item.id}>
+//                         <Link to={`/item/${item.id}`}>
+//                             <h3>{item.title}</h3>
+//                             <p>${item.price}</p>
+//                             <p>{item.categoryId}</p>
+//                         </Link>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// };
+
+// ItemList.propTypes = {
+//     items: propTypes.array.isRequired,
+//     isLoading: propTypes.bool,
+// };
+
+// export default ItemList;
+
+
 import propTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const ItemList = ({ items, isLoading }) => {
-    if (isLoading) {
-        return <h2>Cargando...</h2>;
-    }
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
-    return (
-        <div>
-            <h1>ItemList</h1>
+  return (
+    <div>
+      <h1>ItemList</h1>
 
-            <Link to="/checkout">Comprar</Link>
-
-            <ul>
-                {items.map((item) => (
-                    <li key={item.id}>
-                        <Link to={`/item/${item.id}`}>
-                            <h3>{item.title}</h3>
-                            <p>${item.price}</p>
-                            <p>{item.categoryId}</p>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <Link to={`/item/${item.id}`}>
+              <h3>{item.name}</h3>
+              <p>${item.price}</p>
+              <p>{item.category}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 ItemList.propTypes = {
-    items: propTypes.array.isRequired,
-    isLoading: propTypes.bool,
+  items: propTypes.array.isRequired,
+  isLoading: propTypes.bool,
 };
 
 export default ItemList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-
-// import { initializeApp } from "firebase/app";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBVn1Cx1nQkr3ngKlQeOkf9xTSjF89Lkuo",
-//   authDomain: "proyecto-final-coder-rea-3fcaf.firebaseapp.com",
-//   projectId: "proyecto-final-coder-rea-3fcaf",
-//   storageBucket: "proyecto-final-coder-rea-3fcaf.appspot.com",
-//   messagingSenderId: "288594795764",
-//   appId: "1:288594795764:web:2268e84f1c7a54d2a692a1"
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { initializeApp } from "firebase/app";
